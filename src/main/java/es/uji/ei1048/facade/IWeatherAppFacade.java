84189d56917f73f46a7b9cd813516263bcdb228d
GuillermoHRT <guillermohrt@gmail.com>
2020-01-12 20:18:53 +0100

    Facade arreglada

--- src/main/java/es/uji/ei1048/facade/IWeatherAppFacade.java
+++ src/main/java/es/uji/ei1048/facade/IWeatherAppFacade.java
@@ -8,10 +8,8 @@ import es.uji.ei1048.exceptions.InvalidCoordenatesException;
 import java.util.List;
 
 public interface IWeatherAppFacade {
-    CondicionesMeteorologicas getCondicionesActuales();
     CondicionesMeteorologicas getCondicionesActuales(String ciudad) throws InvalidCityException;
     CondicionesMeteorologicas getCondicionesActuales(Coordenadas coord) throws InvalidCoordenatesException;
-    List<CondicionesMeteorologicas> getPrediccion();
     List<CondicionesMeteorologicas> getPrediccion(String ciudad) throws InvalidCityException;
     List<CondicionesMeteorologicas> getPrediccion(Coordenadas coord) throws InvalidCoordenatesException;
 
--- src/main/java/es/uji/ei1048/facade/WeatherAppFacade.java
+++ src/main/java/es/uji/ei1048/facade/WeatherAppFacade.java
@@ -2,14 +2,13 @@ package es.uji.ei1048.facade;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import es.uji.ei1048.object.CondicionesMeteorologicas;
-import es.uji.ei1048.object.Coordenadas;
 import es.uji.ei1048.WeatherApp;
 import es.uji.ei1048.exceptions.InvalidCityException;
 import es.uji.ei1048.exceptions.InvalidCoordenatesException;
 import es.uji.ei1048.exceptions.InvalidDateException;
+import es.uji.ei1048.object.CondicionesMeteorologicas;
+import es.uji.ei1048.object.Coordenadas;
 import es.uji.ei1048.service.IWeatherService;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 
@@ -26,36 +25,16 @@ public class WeatherAppFacade implements IWeatherAppFacade {
     }
 
     @Override
-    public CondicionesMeteorologicas getCondicionesActuales() {
-
-        // TODO: 27/12/2019 Usado para debug, borrar
-        CondicionesMeteorologicas condicionesMeteorologicas = weatherApp.getCurrentWeather(service);
-        String jsonResult = gson.toJson(condicionesMeteorologicas);
-        System.out.println(jsonResult);
-
-        return condicionesMeteorologicas;
-    }
-
-    @Override
-    public List<CondicionesMeteorologicas> getPrediccion() {
-        List<CondicionesMeteorologicas> prediction = weatherApp.getPredictionWeather(service);
-        String jsonResult = gson.toJson(prediction);
-        System.out.println(jsonResult);
-
-        return prediction;
-    }
-
-    @Override
     public CondicionesMeteorologicas getCondicionesActuales(String ciudad) throws InvalidCityException {
         checkCity(ciudad);
-        return weatherApp.getCurrentWeather(service);
+        return weatherApp.getCurrentWeatherByCity(service, ciudad);
     }
 
     @Override
     public CondicionesMeteorologicas getCondicionesActuales(Coordenadas coord) throws InvalidCoordenatesException {
         // Comprobar ciudad valida
         checkCoordenates(coord);
-        return weatherApp.getCurrentWeather(service);
+        return weatherApp.getCurrentWeatherByCoordenates(service, coord);
     }
 
     @Override
@@ -63,8 +42,7 @@ public class WeatherAppFacade implements IWeatherAppFacade {
         // Comprobar ciudad valida
         checkCity(ciudad);
 
-        //TODO
-        throw new NotImplementedException();
+        return weatherApp.getPredictionWeatherByCity(service, ciudad);
     }
 
     @Override
@@ -72,17 +50,17 @@ public class WeatherAppFacade implements IWeatherAppFacade {
         // Comprobar coordenadas validas
         checkCoordenates(coord);
 
-        // TODO
-        throw new NotImplementedException();
+        return weatherApp.getPredictionWeatherByCoordenates(service, coord);
     }
 
     /**
      * Comprueba que la ciudad exista.
+     *
      * @param city Ciudad introducida por el usuario.
      * @throws InvalidCityException En el caso de que la ciudad no exista.
      */
     private void checkCity(String city) throws InvalidCityException {
-        if (!service.checkCity(city)){
+        if (!service.checkCity(city)) {
             throw new InvalidCityException();
         }
     }
